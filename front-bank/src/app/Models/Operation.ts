a64c515145e5603620bd9100572eb81a758dd6ed
FabioSantCou <fabio.santcou@gmail.com>
2022-04-08 11:20:41 -0300

    created operations Model

---
+++ front-bank/src/app/Models/Operation.ts
@@ -0,0 +1,14 @@
+export default class Operation {
+
+  constructor(
+    public _id:string,
+    public code:string,
+    public description:string,
+    public amount:string,
+    public date:string,
+    public hour:string,
+    public type:string
+  ){}
+
+
+}
